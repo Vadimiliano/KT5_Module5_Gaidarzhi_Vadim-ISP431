3fc869082c8a8fcdfa520851d560f63d19925e9e
FedaSuper <fedorlohkin@gmail.com>
2024-11-21 12:18:21 +0300

    first commit

---
+++ SportApp/SportApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SportApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+
+                if (string.IsNullOrEmpty(LoginTextBox.Text))
+                {
+                    errors.AppendLine("Заполните логин");
+                }
+
+                if (string.IsNullOrEmpty(UserPasswordBox.Password))
+                {
+                    errors.AppendLine("Заполните пароль");
+
+                }
+
+               if(errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+               if(Data.SportAppBDEntities.GetContext().Users.Any(d => d.Login == LoginTextBox.Text && d.Password == UserPasswordBox.Password))
+                {
+                    MessageBox.Show("Успех", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Classes.Manager.MainFrame.Navigate(new Pages.UserListPage());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
